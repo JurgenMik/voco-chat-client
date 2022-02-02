869634d070502c7df816efb75b173bb873ade010
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-02-02 20:28:25 +0200

    Created App style, Created component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {Col} from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
+
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+           <div>
+               <h2> KHK </h2>
+               <div className="ekraanTaust">
+               </div>
+               <Container fluid>
+                   <Row>
+                      <Col className="col-sm-4">
+                          <div className="nupp">
+                              <button type="button" className="saada"> Send </button>
+                          </div>
+                      </Col>
+                   </Row>
+               </Container>
+          </div>
+
+    );
 }
+
 
 export default App;
